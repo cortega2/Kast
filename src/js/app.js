33c952b6c038c920ab22f25f3be5aa09d8c3a2ea
Carlos <corteg20@gmail.com>
2015-07-01 11:52:19 -0500

    broadcasting location and setting the map to that location

--- src/js/app.js
+++ src/js/app.js
@@ -2,32 +2,43 @@
 	var geocoder = new google.maps.Geocoder();
 	var app = angular.module('skycast', ['ui.bootstrap']);
 
-	app.controller('SearchCTRL', function($scope, $http){
+	app.controller('SearchCTRL', function($scope, $rootScope){
 		$scope.selected = undefined;
 		
 		$scope.getLocation = function(){
 			var input = document.getElementById('inputBox');
 			
 			geocoder.geocode( { 'address': input.value}, function(results, status) {
-		    if (status == google.maps.GeocoderStatus.OK) {
-		    	console.log(results);
-		      // map.setCenter(results[0].geometry.location);
-		      // var marker = new google.maps.Marker({
-		      //     map: map,
-		      //     position: results[0].geometry.location
-		      // });
-				input.value = results[0].formatted_address;
-		    } 
-		    	else {
-		      alert('Unable to find address: ' + status);
-		    }
+			    if (status == google.maps.GeocoderStatus.OK) {
+			    	console.log(results);
+			      // map.setCenter(results[0].geometry.location);
+			      // var marker = new google.maps.Marker({
+			      //     map: map,
+			      //     position: results[0].geometry.location
+			      // });
+					input.value = results[0].formatted_address;
+
+					$rootScope.$broadcast('FoundLocation', {
+						lat: results[0].geometry.location.A,
+						lng: results[0].geometry.location.F
+					});
+			    } 
+			    else {
+			      alert('Unable to find address: ' + status);
+			    }
 		  });
 		};		
 	});
 
 	app.controller('MapCTRL', function($scope) {
+		// var container = document.getElementById('mapLocation');
+
 		var map = new LeafletMap('mapLocation')
 		map.init();
+
+		$scope.$on('FoundLocation', function(event, data){
+			map.goTo(data.lat, data.lng);
+		});
 	});
 
 })();
--- src/js/map.js
+++ src/js/map.js
@@ -3,14 +3,18 @@ function LeafletMap(div){
 	var map;
 
 	this.init = function(){
-		map = L.map(div).setView([51.505, -0.09], 13);
+		map = L.map(div).setView([51.505, -0.09], 10);
 
 		var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 		});
 
 		map.addLayer(layer);
-	}
+	};
+
+	this.goTo = function (lat, lng){
+		map.setView([lat,lng], 10, {pan : {animate : true, duration : 1}});
+	};
 
 
 }
